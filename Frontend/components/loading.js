2781c4ecfe5ad2829b2dcf90280b963350de9863
Abhishek Bvs <bvsabhishek@gmail.com>
2020-04-04 21:44:31 +0530

    Fix loading issue

--- Frontend/components/loading.js
+++ Frontend/components/loading.js
@@ -2,9 +2,11 @@ import ReactLoading from 'react-loading';
 
 const loading = () =>{
     return(
-        <div className="loading">
-            <ReactLoading type={"spinningBubbles"} color={"#00000"} height={'5%'} width={'5%'} />
-        </div>
+        <React.Fragment>
+            <div className="loading">
+                <ReactLoading type={"spinningBubbles"} color={"#00000"} height={'5%'} width={'5%'} />
+            </div>
+        </React.Fragment>
     )
 };
 
