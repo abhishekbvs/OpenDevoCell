9c0bc88801c992f4e7497520005be9d36633a15c
Abhishek Bvs <bvsabhishek@gmail.com>
2020-04-16 21:45:01 +0530

    Align elements in loading screen

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -5,6 +5,7 @@ import '../styles/homepage.sass';
 const HomePage = (props) => {
   return (
     <Base loginRequired >
+      <h6>DevoWorm Dashboard</h6>
       <Link href="/analyse/cell-segmentation"><a>Cell Segmentation</a></Link>
     </Base>
   )
